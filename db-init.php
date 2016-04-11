641a1e0d9e6f0d7374c2dc5ce07b357b0b8ee44c
Feaple <jesse.valjakka@luukku.com>
2016-04-11 10:51:58 +0300

    muutoksia lisää

--- db-init.php
+++ db-init.php
@@ -1,7 +1,10 @@
 <?php
-
-	$db = new PDO('mysql:host=178.62.246.71;dbname=database-iizp2010-2;charset=UTF8',
-              'teamh', 'iqo!Ib%nqr35a');
+	$db_host = "178.62.246.71";
+	$db_name = "database-iizp2010-2";
+	$db_user = "teamh";
+	$db_pass = "iqo!Ib%nqr35a";
+	
+	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 ?>
