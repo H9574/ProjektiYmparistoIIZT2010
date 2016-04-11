b85dc01e99c3ab62686afdc816dce87fcfde7cb8
Sanna Räisänen <srisnen11@gmail.com>
2016-04-11 08:48:30 +0300

    Käännökset korjattu

--- Demo.php
+++ Demo.php
@@ -10,7 +10,6 @@
 <script>
   function init() {
     var stage = new createjs.Stage("demoCanvas");
-
         createjs.Ticker.addEventListener("tick", handleTick);
         function handleTick(event) {
     stage.update();
@@ -18,43 +17,32 @@
         // Actions carried out when the Ticker is not paused.
                 }
         }
-
         // piirretään pelin tausta
         var tausta = new Image();
         tausta.src = "MedicineRoom.png"
         var bitmap = new createjs.Bitmap(tausta);
         stage.addChild(bitmap);
-
         var circle = new createjs.Shape();
         circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
         circle.x = 100;
         circle.y = 100;
         stage.addChild(circle);
-
         stage.enableMouseOver();
-
         circle.addEventListener("click", function(event) { alert("clicked"); })
         circle.addEventListener("mouseover", function(event) {d.style.opacity = 1; })
         circle.addEventListener("mouseout", function(event) {d.style.opacity = 0; })
-
         var d = document.getElementById('makkara');
         d.style.position = "absolute";
         d.style.left = "50px";
         d.style.top = "800px";
         d.style.opacity = 0;
         //d.style.z-index = 0;
-
         //var z = document.getElementById('juusto');
         //z.style.position = "absolute";
         //z.style.left = "150px";
         //z.style.top = "100px";
         //d.style.z-index = 1;
-
-
-
         stage.style.position = "absolute";
-
-
         //stage.style.z-index = 0;
   }
 </script>
@@ -65,7 +53,7 @@
         <nav><ul>
 
         <li> <a href="#">Peli näkymä</a> </li>
-        <li> <a href="ulos.php">Kirjaudu ulos</a> </li>
+        <li> <a href="ulos.php">Sign out</a> </li>
 
         </ul></nav>
                 </div>
@@ -80,7 +68,7 @@
                         </header>
 
                     <content >
-                                        <h3>Doctor needs answer for the next question:</h3>
+                                        <h3>Doctor needs the answer for the following question:</h3>
 
 <?php
 require_once 'Laskuja.class.php';
@@ -91,10 +79,8 @@ $RoomaMuunnos = new Roomalaiset();
         $Maksimi = rand(6, 14);
         $Minimi = 4;
         $Vahvuus = array(10, 20);
-
 if ($arpa==1){
         $Aika = rand(1, 48);
-
         $Ratkaisu = $Vastaus->LaskeAnnostusMaksimi($Maksimi, $Aika);
         echo "The drug’s dosage is $Minimi – $Maksimi ml/h, how much of it can you give in a day (or in the $Aika hours)? ";
         echo "<br> Vastaus: $Ratkaisu ml";
@@ -102,7 +88,6 @@ if ($arpa==1){
 }
 if ($arpa==2){
         $Aika = rand(1, 48);
-
         echo "The drug’s dosage is $Minimi – $Maksimi ml/h, how much of it can you give in a day? (or in the $Aika hours)";
         $Ratkaisu = $Vastaus->LaskeAnnostusMinimi($Minimi, $Aika);
         echo "<br> Vastaus: $Ratkaisu ml";
@@ -111,7 +96,6 @@ if ($arpa==2){
         $Ruisku = rand(1, 9)*100;
         $Aika = rand(1, 48);
         $InfuusioNopeus = rand(1, 48);
-
         echo "The drug’s dosage is $Minimi-$Maksimi ml/h, how many $Ruisku ml syringes does the patient need per day (or in $Aika hours) with an infusion rate of $InfuusioNopeus ml/h?";
         $Ratkaisu = $Vastaus->LaskeRuiskujenMaara($Aika, $Ruisku, $InfuusioNopeus);
         echo "<br> Vastaus: $Ratkaisu ml";
@@ -121,7 +105,6 @@ if ($arpa==2){
         $Paino = rand(50, 100);
         $laake = settype($Vahvuus[rand(0,1)], "integer");
         $TarvittavaAntoNopeus = rand(4, 12);
-
         echo "Anaesthesia is maintained to a patient weighting $Paino kg. The drug’s required administration rate is $TarvittavaAntoNopeus mg/kg/h.
 				The drug's strenght is $laake mg/ml. What is the administration rate in unit ml/h?";
         $Ratkaisu = $Vastaus->LaskeAntoNopeus($Paino, $laake, $TarvittavaAntoNopeus);
@@ -130,7 +113,6 @@ if ($arpa==2){
 }if ($arpa==5){
         $KuivaAine = rand(1, 9)*100;
         $Laimennus = rand (10, 50);
-
         echo '<p>Harjoitus 4 ';
         $Ratkaisu = $Vastaus->LaskeNesteMaarasta($KuivaAine, $Laimennus);
         echo "<br> Vastaus: $Ratkaisu</p>";
@@ -138,7 +120,6 @@ if ($arpa==2){
 }if ($arpa==6){
         $KuivaAine = rand(1, 9)*100;
         $Laimennus = rand (10, 50);
-
         echo '<p>Harjoitus 5 ';
         $Ratkaisu = $Vastaus->LaskeNesteMaarasta($KuivaAine, $Laimennus)/10;
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -147,7 +128,6 @@ if ($arpa==2){
         $Annos = rand(1, 6);
         $Paino = rand(50, 100);
         $Aika = rand(1, 4);
-
         echo '<p>Harjoitus 6 ';
         $Ratkaisu = $Vastaus->LaskeNestePainosta($Annos, $Paino, $Aika);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -155,7 +135,6 @@ if ($arpa==2){
 }if ($arpa==8){
         $KuivaAine = rand(1, 9)*100;
         $Liuos = rand(1, 6)*10;
-
         echo '<p>Harjoitus 7 prosentteina';
         $Ratkaisu = $Vastaus->LaskeYksikössäProsentti($KuivaAine, $Liuos);
         echo "<br> Vastaus: $Ratkaisu % </p>";
@@ -163,7 +142,6 @@ if ($arpa==2){
 }if ($arpa==9){
         $KuivaAine = rand(1, 9)*100;
         $Laimennus = rand(1, 6)*10;
-
         echo '<p>Harjoitus 8 ';
         $Ratkaisu = $Vastaus->LaskeVahvuus($KuivaAine, $Laimennus);
         echo "<br>Vastaus: $Ratkaisu mg/ml </p>";
@@ -171,7 +149,6 @@ if ($arpa==2){
 }if ($arpa==10){
         $Laake = rand(6, 30);
         $Vahvuus = 25;
-
         echo '<p>Harjoitus 9 ';
         $Ratkaisu = $Vastaus->LaskeSaatuLaake($Laake, $Vahvuus);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -182,7 +159,6 @@ if ($arpa==2){
         $Paino = rand(50, 100);
         $Injektioliuos = rand(1, 9);
         $Suolaliuos = rand(6, 20);
-
         echo '<p>Harjoitus 10 ';
         $Ratkaisu = $Vastaus->LaskeSaatuLiuos($Laake, $Vahvuus, $Paino, $Injektioliuos, $Suolaliuos);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -191,7 +167,6 @@ if ($arpa==2){
 }if ($arpa==12){
         $KuivaAine = rand(2,20)*10;
         $Liuos = rand(1, 9);
-
         echo '<p>Harjoitus 11 ';
         $Ratkaisu = $Vastaus->LaskeYksikössäProsentti($KuivaAine, $Liuos);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -200,7 +175,6 @@ if ($arpa==2){
         $KuivaAine = rand(10, 20)*10;
         $Suolaliuos = rand(1, 15);
         $Maara = rand(10, 30)*10;
-
         echo '<p>Harjoitus 12 ';
         $Ratkaisu = $Vastaus->LaskeMaara($KuivaAine, $Suolaliuos, $Maara);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -208,7 +182,6 @@ if ($arpa==2){
 }if ($arpa==14){
         $Maara = rand(3, 5);
         $Vahvuus = rand(1, 6);
-
         echo '<p>Harjoitus 13 ';
         $Ratkaisu = $Vastaus->LaskeSaatuMaara($Vahvuus, $Maara);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -216,7 +189,6 @@ if ($arpa==2){
 		}if ($arpa==15){
         $KuivaAine = rand(1, 9)*100;
         $Liuos = rand(1, 6)*10;
-
         echo '<p>Harjoitus 7 ';
         $Ratkaisu = $Vastaus->LaskeYksikössäMgMl($KuivaAine, $Liuos);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -224,7 +196,6 @@ if ($arpa==2){
 }if ($arpa==16){
         $Normi = rand(1, 500);
         $Rooma = $RoomaMuunnos->MuunnaNormiRoomaksi($Normi);
-
         echo '<p>Muuta Roomalainen luvuksi ';
         $Ratkaisu = $RoomaMuunnos->MuunnaRoomaNormiksi($Rooma);
         echo "<br> Vastaus: $Ratkaisu </p>";
@@ -262,6 +233,3 @@ if ($arpa==2){
 </body>
 
 </html>
-
-
-
