b77b0b03d092084b1433efbcedb536576e589bd0
Feaple <jesse.valjakka@luukku.com>
2016-04-11 10:56:45 +0300

    Added files via upload

--- db-init.php
+++ db-init.php
@@ -1,8 +1,8 @@
 <?php
-	$db_host = "178.62.246.71";
+	$db_host = '178.62.246.71';
 	$db_name = "database-iizp2010-2";
 	$db_user = "teamh";
-	$db_pass = "iqo!Ib%nqr35a";
+	$db_pass = 'iqo!Ib%nqr35a';
 	
 	$db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
