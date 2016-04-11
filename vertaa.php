cf351d0af06f4be722e8d7115df8c822b12cadb7
Sanna Räisänen <srisnen11@gmail.com>
2016-04-11 11:34:46 +0300

    toimiikos

--- vertaa.php
+++ vertaa.php
@@ -1,29 +1,11 @@
 <?php
-/*require_once 'Laskuja.class.php';
-$Vastaus = new Laskuja();
-$Oppilas = $_POST["ruisku"];
-
-$Maksimi = 4;
-$Minimi = 6;
-$Ruisku = 50;
-$Aika = 24;
-$InfuusioNopeus = 5;
-
-$Ratkaisu = $Vastaus->LaskeRuiskujenMaara($Aika, $Ruisku, $InfuusioNopeus);
-
-if ($Oppilas == $Ratkaisu){
+$Vastaus = $_POST['vastaus'];
+$Ratkaisu = $_POST['ratkaisu'];
+if ($Vastaus == $Ratkaisu){
         header("Location: oikea.php");
 }
-else{
+else
+{
         header("Location: vaara.php");
-}*/
-$Vastaus = $_POST["vastaus"];
-class Tarkistus($Ratkaisu){
-	if ($Vastaus == $Ratkaisu){
-        header("Location: oikea.php");
-}
-else{
-        header("Location: vaara.php");
-}
 }
 ?>
