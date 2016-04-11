3be3c073296199a91995573416b4aeca94a44ff0
Feaple <jesse.valjakka@luukku.com>
2016-04-11 09:06:07 +0300

    Small change to previous

--- Demo.php
+++ Demo.php
@@ -1,7 +1,7 @@
 <?php
 try{
 	$db = new PDO('mysql:host=178.62.246.71;dbname=database-iizp2010-2;charset=utf8',
-              'teamh', 'iqo!Ib%nqr35a');
+              'teamh', "iqo!Ib%nqr35a");
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 }catch(PDOException $ex){
 	echo "ErrMsg to enduser!<hr>\n";
