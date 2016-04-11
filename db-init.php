5f997438a95b4f39db9249e34ea65b34d235e7b8
Feaple <jesse.valjakka@luukku.com>
2016-04-11 10:43:26 +0300

    Added files via upload

--- db-init.php
+++ db-init.php
@@ -1,5 +1,6 @@
 <?php
-$db = new PDO('mysql:host=178.62.246.71;dbname=database-iizp2010-2;charset=utf8',
+
+	$db = new PDO('mysql:host=178.62.246.71;dbname=database-iizp2010-1;charset=utf8',
               'teamh', 'iqo!Ib%nqr35a');
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
